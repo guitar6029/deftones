cabbb524ceed42c84f00d6fecfa883ce84ed253f
Josh <jsbusiness305@gmail.com>
2022-09-03 19:55:10 -0400

    added hostlistener to resize windwo for menu toggle display

--- src/app/band/nav/nav.component.ts
+++ src/app/band/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +9,25 @@ export class NavComponent implements OnInit {
 
   constructor() { }
 
+  @HostListener("window:resize", ['$event'])
+  onResize(event: any){
+    if(this.toggleDisplay && ( window.innerWidth >= 470 )){
+      this.toggleDisplay = !this.toggleDisplay;
+    }
+    //console.log(window.innerWidth);
+  }
+
+  toggleDisplay: boolean = false;
+  screenWidth? : number;
+
+  toggleMenu(){
+    this.toggleDisplay = !this.toggleDisplay;
+  }
+
+
   ngOnInit(): void {
+    //this.screenWidth = window.innerWidth;
+    //console.log('Screen width : ', this.screenWidth);
   }
 
 }
